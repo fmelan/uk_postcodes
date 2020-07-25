29b8b7735d9704400aedf2eb5368aadcc036a144
František Melán <fmelan@gmail.com>
2020-07-25 15:36:09 +0200

    more tests, wrong postcode

--- tests/test_uk_post_codes.py
+++ tests/test_uk_post_codes.py
@@ -11,6 +11,7 @@ def test_validate():
     assert is_valid("W1A 0AX")
     assert is_valid("M1 1AE")
     assert is_valid("B33 8TH")
+    assert is_valid("B338TH")
     assert is_valid("CR2 6XH")
     assert is_valid("DN55 1PT")
 
@@ -22,3 +23,8 @@ def test_validate():
     assert is_valid("E20 2ST")
     assert is_valid("EC2N 2DB")
     assert is_valid("CO4 3SQ")
+    assert is_valid("BFPO 57")
+    assert is_valid("MSR-2135")
+
+    # czech
+    assert not is_valid("182 00")
